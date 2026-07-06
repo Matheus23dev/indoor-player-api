45434fce624bdfce61f5ed76451f042b8aa181d5
Matheus <87284194mts@gmail.com>
2026-07-06 08:59:52 -0300

    12

--- src/devices/device-auth.guard.ts
+++ src/devices/device-auth.guard.ts
@@ -10,7 +10,7 @@ import type {
 
 import {
   DeviceAuthService,
-} from '../../../../Downloads/indoor-player-api-token-completa/indoor-player-api-token-completa/src/devices/device-auth.service';
+} from './device-auth.service';
 
 import type {
   AuthenticatedDevice,
