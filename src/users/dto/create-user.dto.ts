6793f4b1ab2dfe7e692314a7e9bc82ca8e92effd
icaro.paiva <icaropaiva0112@gmail.com>
2026-07-23 09:45:55 -0300

    ajuste dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,29 @@
-import { UserRole } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+import {
+  UserRole,
+} from '@prisma/client';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password!: string;
+
+  @IsEnum(UserRole)
   role!: UserRole;
 }
